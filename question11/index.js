04ac39e116d2b382d0f4035da30f5bff6271d09b
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2025-06-20 12:12:07 +0530

    added question11

---
+++ question11/index.js
@@ -0,0 +1,4 @@
+// Ques11. File Upload Server with Path Validation
+// Create an HTTP server that allows users to upload files.
+// The server should validate the file path using the path module to ensure that the file is saved within a specific directory (e.g., /uploads).
+// If the file's path is outside the permitted directory, reject the upload.
